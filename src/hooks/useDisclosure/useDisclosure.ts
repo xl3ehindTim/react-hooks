8f16d777655543f7d37e8877c57dc77a77ebc0ac
Tim <timbrouwers1@gmail.com>
2023-07-10 17:24:09 +0200

    Create useDisclosure.ts

---
+++ src/hooks/useDisclosure/useDisclosure.ts
@@ -0,0 +1,17 @@
+import React from "react"
+
+export const useDisclosure = (isOpenDefault = false) => {
+  const [isOpen, setIsOpen] = React.useState(isOpenDefault)
+
+  const onOpen = React.useCallback(() => setIsOpen(true), [])
+  const onClose = React.useCallback(() => setIsOpen(false), [])
+  const onToggle = React.useCallback((toSet: any) => {
+    if (typeof toSet === "undefined") {
+      setIsOpen((state) => !state)
+    } else {
+      setIsOpen(toSet)
+    }
+  }, [])
+
+  return { isOpen, onOpen, onClose, onToggle }
+}
