91e8d1af08638328735cdd88f565d20a564c2682
Tim <timbrouwers1@gmail.com>
2023-07-10 17:25:33 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { useNavigatorLanguage } from './hooks/useNavigatorLanguage/useNavigatorLanguage'
 export { useCookie } from './hooks/useCookie/useCookie'
 export { useLocalStorage } from './hooks/useLocalStorage/useLocalStorage'
+export { useDisclosure } from './hooks/useDisclosure/useDisclosure'
